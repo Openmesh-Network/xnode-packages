07de13b973351874bd99d02ade971fc5458059f4
Plopmenz <plopmenz@gmail.com>
2026-05-14 18:45:46 +0200

    add vllm omni

---
+++ vllm-omni/xpu/flake.nix
@@ -0,0 +1,75 @@
+{
+  inputs = {
+    vllm.url = "path:../../vllm/xpu";
+    xnode-builders.follows = "vllm/xnode-builders";
+    nixpkgs.follows = "vllm/nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    let
+      name = "vllm-omni";
+      version = "0.20.0";
+    in
+    inputs.xnode-builders.language.python {
+      inherit name version;
+      getArgs =
+        { pkgs, ... }:
+        {
+          src = pkgs.stdenv.mkDerivation {
+            inherit version;
+            name = "vllm-omni-patched";
+            src = pkgs.fetchFromGitHub {
+              owner = "vllm-project";
+              repo = "vllm-omni";
+              rev = "v${version}";
+              hash = "sha256-+/58hW+nYJeyDHy5oarJXro+fqPuOIMheN0slaQmnoU=";
+            };
+            dontConfigure = true;
+            dontBuild = true;
+            installPhase = ''
+              mkdir -p $out
+              mv ./* $out
+              cp ${./uv.lock} $out/uv.lock
+            '';
+          };
+
+          extraPackageArgs.override = pkgs.lib.composeManyExtensions [
+            inputs.vllm.extras.${pkgs.stdenv.hostPlatform.system}.overlay
+            inputs.vllm.extras.${pkgs.stdenv.hostPlatform.system}.args.extraPackageArgs.override
+            (final: prev: {
+              vllm-omni = prev.vllm-omni.overrideAttrs (old: {
+                env = (old.env or { }) // {
+                  VLLM_OMNI_TARGET_DEVICE = "xpu";
+                  VLLM_OMNI_VERSION_OVERRIDE = version;
+                };
+              });
+
+              antlr4-python3-runtime = prev.antlr4-python3-runtime.overrideAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [
+                  final.setuptools
+                ];
+              });
+
+              openai-whisper = prev.openai-whisper.overrideAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [
+                  final.setuptools
+                ];
+              });
+
+              fa3-fwd = prev.fa3-fwd.overrideAttrs (old: {
+                buildInputs = (old.buildInputs or [ ]) ++ [ final.torch ];
+                autoPatchelfIgnoreMissingDeps = (old.autoPatchelfIgnoreMissingDeps or [ ]) ++ [
+                  "libc10.so"
+                  "libtorch.so"
+                  "libtorch_cpu.so"
+                  "libcudart.so.13"
+                  "libc10_cuda.so"
+                  "libtorch_cuda.so"
+                ];
+              });
+            })
+          ];
+        };
+    };
+}
